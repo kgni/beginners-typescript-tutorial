00f378ec992f54269aa368ed84ccb8f44f092a7b
kgni <admin@mdia.dk>
2022-10-28 11:59:28 +0200

    add solution 14

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -1,4 +1,4 @@
-import { Equal, Expect } from "./helpers/type-utils";
+import { Equal, Expect } from './helpers/type-utils';
 
 /**
  * Here, the id property is shared between all three
@@ -6,25 +6,25 @@ import { Equal, Expect } from "./helpers/type-utils";
  * make it more DRY?
  */
 
-interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
+interface base {
+	id: string;
+}
+interface User extends base {
+	firstName: string;
+	lastName: string;
 }
 
-interface Post {
-  id: string;
-  title: string;
-  body: string;
+interface Post extends base {
+	title: string;
+	body: string;
 }
 
-interface Comment {
-  id: string;
-  comment: string;
+interface Comment extends base {
+	comment: string;
 }
 
 type tests = [
-  Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
-  Expect<Equal<Post, { id: string; title: string; body: string }>>,
-  Expect<Equal<Comment, { id: string; comment: string }>>,
+	Expect<Equal<User, { id: string; firstName: string; lastName: string }>>,
+	Expect<Equal<Post, { id: string; title: string; body: string }>>,
+	Expect<Equal<Comment, { id: string; comment: string }>>
 ];
